8994e240f9399cbaea0ab7b35d47749329ff7d57
Andrew Kovachik <andrew.kovachik@canada.ca>
2018-03-23 14:47:05 +0000

    fixed searching for files from changed directory

--- bashFiles/dataRoutines.sh
+++ bashFiles/dataRoutines.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-/home/akovachi/anaconda3/bin/python /home/akovachi/lrt_data/checkFiles.py
-/home/akovachi/anaconda3/bin/python /home/akovachi/lrt_data/rt1hz.py
-/home/akovachi/anaconda3/bin/python /home/akovachi/lrt_data/recordlrt.py
-/home/akovachi/anaconda3/bin/python /home/akovachi/lrt_data/graph.py
+/home/akovachi/anaconda3/bin/python /home/akovachi/lrt_data/src/checkFiles.py
+/home/akovachi/anaconda3/bin/python /home/akovachi/lrt_data/src/rt1hz.py
+/home/akovachi/anaconda3/bin/python /home/akovachi/lrt_data/src/recordlrt.py
+/home/akovachi/anaconda3/bin/python /home/akovachi/lrt_data/src/graph.py
