465c7fbdb64e9f1fb0e404030f380b5e3bc3b1a6
Andrew Kovachik <andrew.kovachik@canada.ca>
2018-04-24 18:51:41 +0000

    initial commit

--- bashFiles/dataRoutines.sh
+++ bashFiles/dataRoutines.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-/home/akovachi/anaconda3/bin/python /home/akovachi/lrt_data/src/checkFiles.py
-/home/akovachi/anaconda3/bin/python /home/akovachi/lrt_data/src/rt1hz.py
-/home/akovachi/anaconda3/bin/python /home/akovachi/lrt_data/src/recordlrt.py
-/home/akovachi/anaconda3/bin/python /home/akovachi/lrt_data/src/graph.py
+$HOME/anaconda3/bin/python $HOME/lrt_data/src/checkFiles.py
+$HOME/anaconda3/bin/python $HOME/lrt_data/src/rt1hz.py
+$HOME/anaconda3/bin/python $HOME/lrt_data/src/recordlrt.py
+$HOME/anaconda3/bin/python $HOME/lrt_data/src/graph.py
