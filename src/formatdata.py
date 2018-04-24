c186b3940d139f8a36cef71fe931304178bf9bd8
Andrew Kovachik <andrew.kovachik@canada.ca>
2018-04-24 14:28:06 +0000

    removed an old print statement

--- src/formatdata.py
+++ src/formatdata.py
@@ -112,7 +112,6 @@ class Data():
                 columns = ['date', 'time', 'doy', 'x', 'y', 'z', 'f']
                 data_frame = pd.read_fwf(self.file, header=None, names=columns)
                 t = datetime.strptime(data_frame['time'][0], '%H:%M:%S:%f')
-                print(t)
                 data_frame['time'] = data_frame['time'].map(
                     lambda x:
                     float((datetime.strptime(x, '%H:%M:%S:%f') - t
