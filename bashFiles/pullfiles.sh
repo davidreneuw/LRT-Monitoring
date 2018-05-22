1ac78e8f11c7b2284f18cf3f7ac870f64b7c742d
dcalp <david.calp@canada.ca>
2018-05-22 19:03:35 +0000

    path changed to include git folder.

--- bashFiles/pullfiles.sh
+++ bashFiles/pullfiles.sh
@@ -4,4 +4,4 @@ echo "retrieving data from OTT "
 
 year=$(date +"%Y")
 
-rsync -azOv 132.156.100.222:/geomag_data/geomag_data/real_time/magnetic/OTT2018* $HOME/cRio-data-reduction/ottSecData/$year
+rsync -azOv geomag_ops@132.156.100.222:/daqs1_data/geomag_data/real_time/magnetic/OTT2018* $HOME/git/crio-data-reduction/ottSecData/$year
