abfb4f48dbc3648424fff60a4584f99aa24826a3
David René <david.rene@nrcan-rncan.gc.ca>
2021-09-29 13:34:03 +0000

    Updated config file as well as checkfiles.py to implement dev mode. In addition I have added a pulldevfiles.sh script that should be run instead of pullfiles.sh when developping. I will add documentation about that in the readme and what to run instead of getLrtData.sh

---
+++ bashFiles/pulldevfiles.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "retrieving 2020 data from OTT "
+
+year=2020
+
+rsync -azOv --timeout=60 --bwlimit=100 drene@ssh.seismo.nrcan.gc.ca://daqs/geomag_data/GSC/real_time/magnetic/$year/OTT$year* /$HOME/crio-data-reduction/ottSecData/$year
